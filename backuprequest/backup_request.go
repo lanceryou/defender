dd8825898175bb0c9af9f1c94dd6eb27ff10817b
lanceryou <lanceryou@tencent.com>
2021-05-17 11:37:12 +0800

    fix bug- protect response,avoid data race

--- backuprequest/backup_request.go
+++ backuprequest/backup_request.go
@@ -2,13 +2,38 @@ package backuprequest
 
 import (
 	"context"
+	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
 type Backup struct {
 	BackupRequestMs time.Duration
 }
 
+type BackupResult struct {
+	Ret unsafe.Pointer
+}
+
+func (r BackupResult) Cas(new unsafe.Pointer) bool {
+	return atomic.CompareAndSwapPointer(&r.Ret, r.Ret, new)
+}
+
+// 业务自己做资源保护
+/*
+ * var ret int
+ * Backup{time.Second}.Execute(ctx, func() error{
+ *        tmp, err := apply.exec()
+ *        if err != nil{
+ *              return err
+ *        }
+ *        if BackupResult{Ret:unsafe.Pointer(&ret)}.Cas(unsafe.Pointer(&tmp)){
+ *                // has response, do not
+ *                return err
+ *        }
+ *        return err
+ * })
+ */
 func (b *Backup) Execute(ctx context.Context, fn func() error) error {
 	errChan := make(chan error)
 	async := func() { errChan <- fn() }
